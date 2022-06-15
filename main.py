e34be89b837364b9d732928c9fe54b5412376852
Alexander Lee <lok.lee19@imperial.ac.uk>
2022-06-15 20:07:51 +0100

    select var frame

--- main.py
+++ main.py
@@ -3,52 +3,44 @@ from tkinter import filedialog, Scrollbar, ttk
 import pandas as pd
 import sys, os
 
+from pyparsing import col
+
 root = Tk()
 root.title('Auto Data Analyser')
 
-open_file_f = LabelFrame(root, text='Select and Preview file', padx=5, pady=5)
+open_file_f = LabelFrame(root, text='Select file', padx=5, pady=5)
 open_file_f.grid(row=0, column=0, padx=10, pady=10)
 
 def select_file():
-    global preview_table
+    global columns, select_file_l
+
     root.filename = filedialog.askopenfilename(initialdir='/Documents', 
         title='Select Excel File', filetypes = [('Excel files', '.xlsx .xls')])
     csv = pd.read_excel(root.filename)
     path = os.path.join(sys.path[0], 'temp/data.csv')
     csv.to_csv(path)
 
+    select_file_l = Label(open_file_f, text=str(root.filename)+' selected', padx=5, pady =5)
+    select_file_l.grid(row=1, column=0)
+
     columns = []
     for c in csv.columns[0:]:
         columns.append(str(c))
 
-    preview_table['columns'] = columns
-
-    preview_table.column('#0', width=0,  stretch=NO)
-    preview_table.heading('#0',text='',anchor=CENTER)
-
-    for c in columns:
-        # preview_table.column(c,anchor=CENTER, width=10)
-        preview_table.heading(c,text=c,anchor=CENTER)
-
-    for k in range(11):
-        values = []
-        for c in columns:
-            values.append(csv.iloc[k][c])
-        print(tuple(values))
-        preview_table.insert(parent='',index='end',iid=k,text='', values = values)
-
-    # my_table.pack(expand = True)    
-
-
-
-
 select_file_b = Button(open_file_f, text='Select Excel File', command=select_file)
 select_file_b.grid(row=0, column=0, padx=5, pady=5)
 
-xscroll = Scrollbar(open_file_f, orient='horizontal')
-xscroll.grid(row=2, column=0, sticky='ew')
+select_file_l = Label(open_file_f, text='No file selected', padx=5, pady =5)
+select_file_l.grid(row=1, column=0)
 
-preview_table = ttk.Treeview(open_file_f, xscrollcommand=xscroll.set)
-preview_table.grid(row=1, column=0, padx=5, pady=5)
+
+select_vars_f = LabelFrame(root, text='Select variables', padx=5, pady=5)
+select_vars_f.grid(row=1, column=0, padx=10, pady=10)
+
+for c in columns:
+    checkbutton = Checkbutton(select_vars_f, text=c)
+    checkbutton.deselect()
+    checkbutton.pack()
+
 
 root.mainloop()
