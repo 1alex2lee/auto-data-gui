dc97bee8c20ab78864838b574b93b747c865aba5
Alex Lee <alexlee@MBP.local>
2022-06-22 11:18:44 +0100

    fix: decision tree

--- decisiontree.py
+++ decisiontree.py
@@ -1,48 +1,91 @@
 from sklearn import tree
 from sklearn.metrics import accuracy_score
-import pandas as pd
-import clean, graphviz
+import graphviz
 from tkinter import *
 from PIL import Image, ImageTk
+from tkinter import messagebox
 
 
 def show(x, y):
+    global max_d, min_i_d
 
-    root = Tk()
+    root = Toplevel()
     root.title('Decision Tree')
 
-    # x, y = clean.up()
-
-    # x = pd.read_csv('temp/data_x.csv', index_col=0)
-    # y = pd.read_csv('temp/data_y.csv', index_col=0)
-
-    dt = tree.DecisionTreeClassifier(max_depth=5, min_impurity_decrease=0.01)
-    dt.fit(x, y)
-    # print('Accuracy = {}'.format(accuracy_score(y, dt.predict(x))))
-
-
-    dot_data = tree.export_graphviz(dt, out_file=None) 
-    graph = graphviz.Source(dot_data) 
-
-    predictors = x.columns
-    dot_data = tree.export_graphviz(dt, out_file=None,
-                                    feature_names = predictors,
-                                    class_names = ('Negative', 'Positive'),
-                                    filled = True, rounded = True,
-                                    special_characters = True)
-    graph = graphviz.Source(dot_data)  
-    graph
-
-    graph.format = 'png'
-    graph.render('temp/decisiontree')
-
-
     Label(root, 
-        text="A decision tree uses a selection algorithm to select variables in a predictive model."
-        ).pack()
+    text="A decision tree uses a selection algorithm to select variables for a predictive model.\nIt requires 2 parameters as below. Feel free to change these parameters."
+    ).grid(row=0, column=0, columnspan=3)
 
-    image = Image.open('temp/decisiontree.png')
-    display = ImageTk.PhotoImage(image)
-    Label(root, image=display).pack()
+    Label(root, text="Max depth (integer > 2, default = 5)").grid(row=1, column=0)
+    Label(root, text="Min impurity increase (0.1-0.001, default = 0.01)").grid(row=1, column=1)
+
+    max_d = Entry(root)
+    max_d.insert(END, '5')
+    max_d.grid(row=2, column=0)
+
+    min_i_d = Entry(root)
+    min_i_d.insert(END, '0.01')
+    min_i_d.grid(row=2, column=1)
+
+
+    tree_f = Frame(root)
+    tree_f.grid(row=3, column=0, columnspan=3)
+
+
+    def create_tree(event=None):
+
+        for widgets in tree_f.winfo_children():
+            widgets.destroy()
+
+        # x, y = clean.up()
+
+        # x = pd.read_csv('temp/data_x.csv', index_col=0)
+        # y = pd.read_csv('temp/data_y.csv', index_col=0)
+
+        try:
+            max_depth = int(max_d.get())
+            min_i_decrease = float(min_i_d.get())
+        except:
+            messagebox.showerror('Error', "You didn't input a number.")
+            return
+
+        try:
+            dt = tree.DecisionTreeClassifier(
+                max_depth=max_depth, 
+                min_impurity_decrease=min_i_decrease)
+            dt.fit(x, y)
+            # print('Accuracy = {}'.format(accuracy_score(y, dt.predict(x))))
+        except:
+            messagebox.showerror('Error', "Parameters out of range.")
+            return
+
+        dot_data = tree.export_graphviz(dt, out_file=None) 
+        graph = graphviz.Source(dot_data) 
+
+        predictors = x.columns
+        dot_data = tree.export_graphviz(dt, out_file=None,
+                                        feature_names = predictors,
+                                        class_names = ('Negative', 'Positive'),
+                                        filled = True, rounded = True,
+                                        special_characters = True)
+        graph = graphviz.Source(dot_data)  
+        graph
+
+        graph.format = 'png'
+        graph.render('temp/decisiontree')
+
+
+        image = Image.open('temp/decisiontree.png')
+        display = ImageTk.PhotoImage(image)
+        photo_label = Label(tree_f, image=display)
+        photo_label.image = display
+        photo_label.pack()
+
+
+
+    root.bind('<Return>', create_tree)
+
+    Button(root, text='Create Tree', command=create_tree).grid(row=1, column=2, rowspan=2)
+
 
     root.mainloop()
